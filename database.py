eb556aebfe9ac00dfc59d7e37338a605a2d40e5a
Wessie <Wessie@Wessie.info>
2012-11-10 11:41:11 +0100

    Added database.py

---
+++ database.py
@@ -0,0 +1,36 @@
+import MySQLdb
+import MySQLdb.cursors
+import config
+
+
+class MySQLCursor:
+    """Return a connected MySQLdb cursor object"""
+    counter = 0
+    cache = {}
+    def __init__(self, cursortype=MySQLdb.cursors.DictCursor, lock=None):
+        threadid = current_thread().ident
+        if (threadid in self.cache):
+            self.conn = self.cache[threadid]
+            self.conn.ping(True)
+        else:
+            self.conn = MySQLdb.connect(host=config.dbhost,
+                                user=config.dbuser,
+                                passwd=config.dbpassword,
+                                db=config.dbtable,
+                                charset='utf8',
+                                use_unicode=True)
+            self.cache[threadid] = self.conn
+        self.curtype = cursortype
+        self.lock = lock
+    def __enter__(self):
+        if (self.lock != None):
+            self.lock.acquire()
+        self.cur = self.conn.cursor(self.curtype)
+        return self.cur
+        
+    def __exit__(self, type, value, traceback):
+        self.cur.close()
+        self.conn.commit()
+        if (self.lock != None):
+            self.lock.release()
+        return
